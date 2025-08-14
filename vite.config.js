f093ab630c262965c7b4504260dd69cd9265a75f
marcnel panganiban <marcnelpanganiban0808@gmail.com>
2025-08-15 07:27:40 +0800

    change base

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.VITE_BASE_PATH || '/Portfolio', // Adjust this if your project is hosted in a subdirectory
+  base: process.env.VITE_BASE_PATH || '/', // Adjust this if your project is hosted in a subdirectory
 })
